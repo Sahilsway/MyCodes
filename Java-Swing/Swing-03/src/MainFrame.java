9809eeb69f0bb94c06fc1c4161bd6e477fb49063
Sahil Swargiary <sahilswargiary.personal@gmail.com>
2022-11-13 00:10:46 +0530

    Initial learning project files of java swing gui

---
+++ Java-Swing/Swing-03/src/MainFrame.java
@@ -0,0 +1,40 @@
+
+import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MainFrame {
+
+    // Create a new private type JFrame
+    private JFrame frame;
+    
+    // Constructor to call frame initialization methode
+    public MainFrame() {
+        initialize();
+    }
+    // Frame atributes
+    private void initialize() {
+        frame = new JFrame();
+        frame.setLayout(new BorderLayout(10, 5));
+        frame.setTitle("Swing JPanel");
+        frame.setSize(800, 500);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        // Create a new JPanel
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 5));
+
+        // Background color of the panel
+        panel.setBackground(Color.RED);
+
+        // Creating button component and adding to the panel
+        Button btn = new Button("Click me!");
+        panel.add(btn);
+        
+        // Add JPanel to JFrame
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setVisible(true);
+    }
+    
+}
---
+++ Java-Swing/Swing-04/src/Launcher.java
@@ -0,0 +1,19 @@
+
+import javax.swing.SwingUtilities;
+
+public class Launcher {
+    
+    public static void main(String[] args) {
+        
+        SwingUtilities.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                
+                MainFrame frame = new MainFrame();
+                
+            }
+
+        });
+    }
+}
